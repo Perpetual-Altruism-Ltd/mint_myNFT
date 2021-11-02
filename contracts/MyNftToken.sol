cebf437cbbfa9b7dc9119d668a1093af6eabe597
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2021-11-02 14:25:21 +0100

    Created ERC271 contract
    Created project with truffle and connected it with remix through remixd. In the project you can find ERC271 contract and MyNftToken contract.

---
+++ contracts/MyNftToken.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.8.6;
+
+import "./Erc721.sol";
+
+contract MyNftToken is ERC721 {
+    // Token name
+    string private _name;
+
+    // Token symbol
+    string private _symbol;
+
+    // All our nft tokens
+    string[] public nftTokens;
+
+    mapping(string => bool) _nameOfNftExists;
+
+    // Check if we can access the length from front-end
+    // Total number of tokens in block chain
+    // uint public totalSupply =0;
+
+    constructor() {
+        _name = "myNFTToken";
+        _symbol = "myNFTSymbol";
+    }
+
+    // Names for our nft to be unique
+    modifier uniqueName(string memory _nameOfNft) {
+        require(!_nameOfNftExists[_nameOfNft]);
+        _;
+    }
+
+    function mint(string memory _nameOfNft) external uniqueName(_nameOfNft) {
+        nftTokens.push(_nameOfNft);
+        uint256 id = nftTokens.length - 1;
+        _mint(msg.sender, id);
+        _nameOfNftExists[_nameOfNft] = true;
+        // totalSupply++;
+    }
+}
