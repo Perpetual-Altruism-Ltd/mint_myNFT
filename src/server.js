5ad5f056ef17eb6386d5575b4ecd51e46f532d94
Delali <michaelazumah@live.com>
2021-11-11 18:35:34 +0000

    create new server

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import express from 'express'
+import compression from 'compression'
+import cors from 'cors'
+
+const app = express()
+
+const __filename = fileURLToPath( import.meta.url )
+const __dirname = dirname( __filename )
+
+const https_redirect = function ( req, res, next ) {
+  if ( req.headers['x-forwarded-proto'] != 'https' ) {
+    return res.redirect( 'https://' + req.headers.host + req.url )
+  } else {
+    return next()
+  }
+}
+
+// app.use(https_redirect)
+app.use(cors())
+app.use( compression() )
+
+// in the dist directory
+app.use( '/', express.static( __dirname + '/ui' ) )
+// Start the app by listening on the default
+// app.get( '/*', function ( req, res ) {
+//   const applicationPath = path.join( __dirname + '/src/ui/index.html' )
+//   res.sendFile( applicationPath )
+// } )
+
+const PORT = process.env.PORT || 3000
+
+app.listen( PORT, () => {
+  console.log( `App listening on port ${PORT}.` )
+} )
